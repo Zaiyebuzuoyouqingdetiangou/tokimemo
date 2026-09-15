eeb619209dd53896666c5fd3bba118838f6cfc98
Zaiyebuzuoyouqingdetiangou <xinyuezhang847@gmail.com>
2026-09-15 09:20:24 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const VERSION = '0.8.79';
-const BUILD = '0.8.79-tt-cg-r84.1';
+const VERSION = '0.8.80';
+const BUILD = '0.8.80-tt-cg-r84.2';
 
 const SETTINGS_ID = 'heartbeat_memories_settings';
 const MENU_ID = 'heartbeat_memories_menu_item';
